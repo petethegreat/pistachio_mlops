9d9282e270cc49de71558b26a052bf4eee3a14b1
Pete <petethegreat@gmail.com>
2023-11-26 18:31:05 -0500

    component definitions with image uri placeholders

--- pipeline/pipeline_definition/pipeline.py
+++ pipeline/pipeline_definition/pipeline.py
@@ -1,1 +1,6 @@
 # placeholder
+# Either
+#  - whatever runs this deals with placeholders in component yaml
+# Or 
+#  - something else renders the placeholders beforehand, and the pipeline works with defined component paths.
+#  - storage path will be a thing needed here.
