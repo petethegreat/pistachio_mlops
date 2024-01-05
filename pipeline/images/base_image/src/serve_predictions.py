5cbc6bfe645561649069e200af33dfebbbd2bd7c
Pete <petethegreat@gmail.com>
2024-01-04 22:00:23 -0500

    update

--- pipeline/images/base_image/src/serve_predictions.py
+++ pipeline/images/base_image/src/serve_predictions.py
@@ -2,6 +2,29 @@
 from fastapi import FastAPI
 
 from google.cloud import storage
+from pydantic import BaseModel
+
+class InferenceRecord(BaseModel):
+    AREA: float
+    PERIMETER:  float
+    MAJOR_AXIS:  float
+    MINOR_AXIS:  float
+    ECCENTRICITY:  float
+    EQDIASQ:  float
+    SOLIDITY:  float
+    CONVEX_AREA:  float
+    EXTENT:  float
+    ASPECT_RATIO:  float
+    ROUNDNESS:  float
+    COMPACTNESS:  float
+    SHAPEFACTOR_1:  float
+    SHAPEFACTOR_2:  float
+    SHAPEFACTOR_3:  float
+    SHAPEFACTOR_4:  float
+
+class InferenceBody(BaseModel):
+    instances: List[InferenceRecord]
+
 
 
 app = FastAPI()
@@ -20,12 +43,13 @@ def get_model_artifacts(path: str):
 # https://cloud.google.com/vertex-ai/docs/predictions/custom-container-requirements#prediction
 
 @app.post("/predict")
-async def predict(instances, parameters):
+async def predict(instances):
     """handles the request, return prediction"""
 
 
 @app.get("/health")
-async def predict(instances, parameters):
+async def predict(InferenceBody):
     """handles the request, return prediction"""   
     # either respond 200 if ready to receive prediction requests, or 503 if not.
+    
 
